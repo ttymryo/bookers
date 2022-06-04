ae3e4625991e09395b4764144862174f1d879ee0
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-06-04 04:54:17 +0000

    [add]book.update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def book_params
