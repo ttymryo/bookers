e7622d1e3a1717dfc4e66f360f2ba020fa37b369
RyotoTsuchiyama <106653745+ttymryo@users.noreply.github.com>
2022-06-04 05:38:58 +0000

    [Add]flash.message

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books =Book.all
@@ -29,6 +30,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
       redirect_to book_path(@book.id)
     else
       render :edit
@@ -38,6 +40,7 @@ class BooksController < ApplicationController
   def destroy
     book =Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
 
